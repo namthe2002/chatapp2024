72cb29441c442c833762b1db426dafb83e1c4ca6
The Nam <ehtNam@macs-iMac.lan>
2024-09-09 09:06:02 +0700

    update 2

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_zh.dart
@@ -0,0 +1,668 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Chinese (`zh`).
+class AppLocalizationsZh extends AppLocalizations {
+  AppLocalizationsZh([String locale = 'zh']) : super(locale);
+
+  @override
+  String get username => '用户名';
+
+  @override
+  String get password => '密码';
+
+  @override
+  String get welcome => '欢迎来到 ChatAPP';
+
+  @override
+  String get login_content => '立即登录以探索功能并与朋友联系。';
+
+  @override
+  String get forgotpass => '忘记密码？';
+
+  @override
+  String get login_error => '登录错误';
+
+  @override
+  String get login => '登录';
+
+  @override
+  String get chatApp => 'ChatApp';
+
+  @override
+  String get my_account => '我的账户';
+
+  @override
+  String get friend => '朋友';
+
+  @override
+  String get invite_friend => '好友请求';
+
+  @override
+  String get night_mode => '夜间模式';
+
+  @override
+  String get chat_setting => '聊天设置';
+
+  @override
+  String get notification => '通知';
+
+  @override
+  String get language => '语言';
+
+  @override
+  String get log_out => '登出';
+
+  @override
+  String get welcome_chat => '欢迎来到 ChatApp';
+
+  @override
+  String get welcome_content => '一个帮助您保护隐私的消息应用程序，与朋友和组织开始聊天。';
+
+  @override
+  String get create_chat => '创建聊天';
+
+  @override
+  String get pin => '置顶';
+
+  @override
+  String get delete_chat => '删除聊天';
+
+  @override
+  String get selct_chat => '选择多个聊天';
+
+  @override
+  String get cancel => '取消';
+
+  @override
+  String get delete => '删除';
+
+  @override
+  String get light_mode => '浅色模式';
+
+  @override
+  String get dark_mode => '深色模式';
+
+  @override
+  String get auto_mode => '自动模式';
+
+  @override
+  String get select => '已选择';
+
+  @override
+  String get account => '账户';
+
+  @override
+  String get settings => '设置';
+
+  @override
+  String get chat_delete => '永久删除与';
+
+  @override
+  String get search_chat => '搜索聊天，群组';
+
+  @override
+  String get recent => '最近';
+
+  @override
+  String get clear => '清除';
+
+  @override
+  String get new_group => '新群组';
+
+  @override
+  String get friends => '朋友';
+
+  @override
+  String get choose_member => '选择成员';
+
+  @override
+  String get auto_delete_chat => '自动删除消息';
+
+  @override
+  String get off => '关闭';
+
+  @override
+  String get auto_delete_chat_content => '在一段时间后自动删除此群组中的消息';
+
+  @override
+  String get members => '成员';
+
+  @override
+  String get create => '创建';
+
+  @override
+  String get auto_delete_title => '自动删除消息';
+
+  @override
+  String get auto_delete_content => '为您的方便启用了自动删除';
+
+  @override
+  String get day => '天';
+
+  @override
+  String get days => '天';
+
+  @override
+  String get update_profile => '更新资料';
+
+  @override
+  String get update_profile_content => '更新您的资料有助于朋友更好地识别您。';
+
+  @override
+  String get person_information => '个人信息';
+
+  @override
+  String get first_name => '名字';
+
+  @override
+  String get last_name => '姓氏';
+
+  @override
+  String get update => '更新';
+
+  @override
+  String get search_friend => '搜索朋友';
+
+  @override
+  String get received => '已接收';
+
+  @override
+  String get sent => '已发送';
+
+  @override
+  String get accept => '接受';
+
+  @override
+  String get prieview => '预览';
+
+  @override
+  String get mesage_text_size => '消息文字大小';
+
+  @override
+  String get color_theme => '颜色主题';
+
+  @override
+  String get chat_notification => '聊天通知';
+
+  @override
+  String get notification_content => '有新通知时接收推送通知';
+
+  @override
+  String get translate_mesages => '翻译消息';
+
+  @override
+  String get translate_content => '选择您想翻译消息的语言';
+
+  @override
+  String get laguage_systems => '语言系统';
+
+  @override
+  String get language_translate => '语言翻译';
+
+  @override
+  String get language_conttent => '选择您想翻译消息的语言';
+
+  @override
+  String get type_a_message => '输入消息...';
+
+  @override
+  String get no_message => '此聊天中没有消息。';
+
+  @override
+  String get media => '照片和视频';
+
+  @override
+  String get sharelink => '分享链接';
+
+  @override
+  String get document => '文档';
+
+  @override
+  String get atuto_delete => '自动删除';
+
+  @override
+  String get clear_history => '清除历史记录';
+
+  @override
+  String get leave_group => '离开群组';
+
+  @override
+  String get reply => '回复';
+
+  @override
+  String get copy => '复制';
+
+  @override
+  String get forward => '转发';
+
+  @override
+  String get recall => '撤回';
+
+  @override
+  String get edit => '编辑';
+
+  @override
+  String get multiple_select => '多选';
+
+  @override
+  String get error_name => '名称不能为空';
+
+  @override
+  String get error_message => '消息列表错误通知';
+
+  @override
+  String get error_file => '文件错误通知';
+
+  @override
+  String get error_delete_message => '删除消息错误通知';
+
+  @override
+  String get error_login => '登录错误通知';
+
+  @override
+  String get file_notification => '文件通知';
+
+  @override
+  String get error_image_video => '只能分别添加图片或视频';
+
+  @override
+  String get error_update => '只能添加一个视频';
+
+  @override
+  String get add_friend_ss => '已成为朋友！';
+
+  @override
+  String get no_add_friend => '好友请求被拒绝！';
+
+  @override
+  String get no_access => '无访问权限';
+
+  @override
+  String get video => '视频';
+
+  @override
+  String get image => '图片';
+
+  @override
+  String get file => '文件';
+
+  @override
+  String get audio => '音频';
+
+  @override
+  String get enter_message => '输入消息';
+
+  @override
+  String get downloading => '下载中...';
+
+  @override
+  String get choose_mode => '选择适合您的界面模式';
+
+  @override
+  String get delete_message => '已删除聊天！';
+
+  @override
+  String get group_name => '群组名称';
+
+  @override
+  String get error_api_no_connect => '无法连接到服务器，请重新检查。';
+
+  @override
+  String get unlock_friend => '解锁发送\n好友请求';
+
+  @override
+  String get translater => '翻译';
+
+  @override
+  String get admin_create => '创建了群组';
+
+  @override
+  String get cancel_friend => '取消好友';
+
+  @override
+  String get cancal_friend_with => '取消与';
+
+  @override
+  String get cancal_friend_ss => '取消了好友';
+
+  @override
+  String get add_friend => '添加好友';
+
+  @override
+  String get leader => '群主';
+
+  @override
+  String get add_member => '添加成员';
+
+  @override
+  String get friend_sent => '好友请求已发送';
+
+  @override
+  String get me => '我';
+
+  @override
+  String get confirm => '确认';
+
+  @override
+  String get delete_group => '删除群组';
+
+  @override
+  String get leave_group_confirm => '您确定要离开并删除此群组聊天吗？';
+
+  @override
+  String get delete_group_confirm => '您确定要删除此群组聊天吗？';
+
+  @override
+  String get add_account => '添加账户';
+
+  @override
+  String get you_sure_loguot => '您确定要登出吗？';
+
+  @override
+  String get email => '电子邮件';
+
+  @override
+  String get admin => '管理员';
+
+  @override
+  String get user => '用户';
+
+  @override
+  String get user_name_empty => '用户名不能为空';
+
+  @override
+  String get email_empty => '电子邮件不能为空';
+
+  @override
+  String get last_name_empty => '姓氏不能为空';
+
+  @override
+  String get name_empty => '名称不能为空';
+
+  @override
+  String get detail => '详细信息';
+
+  @override
+  String get select_filter => '选择您想筛选的职位';
+
+  @override
+  String get status => '状态';
+
+  @override
+  String get status_active => '活跃';
+
+  @override
+  String get status_deactive => '已停用';
+
+  @override
+  String get list_account => '账户列表';
+
+  @override
+  String get account_detail => '账户详情';
+
+  @override
+  String get email_format => '电子邮件格式无效';
+
+  @override
+  String get permission_successfully => '权限更改成功';
+
+  @override
+  String get status_successfully => '状态更改成功';
+
+  @override
+  String get lock_account => '锁定账户';
+
+  @override
+  String get unlock_account => '解锁账户';
+
+  @override
+  String get position => '职位';
+
+  @override
+  String get change_pass => '更改密码';
+
+  @override
+  String get pass_old => '输入旧密码';
+
+  @override
+  String get pass_new => '输入新密码';
+
+  @override
+  String get pass_new_en => '重新输入新密码';
+
+  @override
+  String get pass_validate => '密码必须至少8个字符！';
+
+  @override
+  String get pass_ss => '密码更改成功！';
+
+  @override
+  String get pass_valiate_distinctive => '新密码必须与旧密码不同！';
+
+  @override
+  String get pass_valiate_overlap => '重新输入的密码与新密码不匹配！';
+
+  @override
+  String get name_ss => '更新成功！';
+
+  @override
+  String get download => '下载';
+
+  @override
+  String get photo_saved_successfully => '照片已成功保存到图库！';
+
+  @override
+  String get photo_saved_error => '保存照片到图库时出错。';
+
+  @override
+  String get cancel_friend_send => '好友请求已取消';
+
+  @override
+  String get is_typing => '正在输入';
+
+  @override
+  String get file_size => '文件大小超过20 MB';
+
+  @override
+  String get un_pin => '取消置顶';
+
+  @override
+  String get search => '搜索';
+
+  @override
+  String get search_nation => '输入国家名称或代码';
+
+  @override
+  String get pinned_message => '已置顶消息';
+
+  @override
+  String get pin_list => '置顶列表';
+
+  @override
+  String get by => '由';
+
+  @override
+  String get back_login => '账户在其他设备上登录';
+
+  @override
+  String get dont_account => '没有账户？';
+
+  @override
+  String get sign_up_here => '在这里注册';
+
+  @override
+  String get sign_up => '注册';
+
+  @override
+  String get we_code_phone => '我们已发送给您手机验证码';
+
+  @override
+  String get otp => 'OTP';
+
+  @override
+  String get information_join => '填写详细信息加入我们';
+
+  @override
+  String get full_name => '全名';
+
+  @override
+  String get enter_password => '重新输入密码';
+
+  @override
+  String get phone_number => '手机号码';
+
+  @override
+  String get label_continue => '继续';
+
+  @override
+  String get phone_empty => '手机号码不能为空';
+
+  @override
+  String get password_empty => '密码不能为空';
+
+  @override
+  String get confirm_password_empty => '确认密码不能为空';
+
+  @override
+  String get phone_format => '手机号码格式无效';
+
+  @override
+  String get auto_delete => '自动删除';
+
+  @override
+  String get otp_no_valid => 'OTP 无效';
+
+  @override
+  String get enter_otp => '输入 OTP';
+
+  @override
+  String get phone_no_valid => '无效的手机号码！';
+
+  @override
+  String get forgot_pass => '忘记密码';
+
+  @override
+  String get forgot_pass_ct => '我们会将链接发送到您之前提供的手机号码。';
+
+  @override
+  String get forgot_pas_phone => '我们已发送代码到您的手机号码';
+
+  @override
+  String get phone => '手机号码';
+
+  @override
+  String get phone_null => '请输入手机号码';
+
+  @override
+  String get phone_valid => '无效的手机号码';
+
+  @override
+  String get pass_null => '请输入密码！';
+
+  @override
+  String get pass_re_null => '请重新输入密码！';
+
+  @override
+  String get pass_reset_ss => '密码重置成功！';
+
+  @override
+  String get resgiter_ss => '账户注册成功！';
+
+  @override
+  String get pass_no_joint => '重新输入的密码不匹配！';
+
+  @override
+  String get last_seen => '最后一次查看';
+
+  @override
+  String get send => '发送';
+
+  @override
+  String get forward_from => '转发自';
+
+  @override
+  String get edited => '已编辑';
+
+  @override
+  String get reset_pass => '重置密码';
+
+  @override
+  String get reset_pass_ss => '密码已重置为123';
+
+  @override
+  String get file_not_supported => '文件不支持';
+
+  @override
+  String get only_photo_video_file => '只能分别添加图片、视频或文件';
+
+  @override
+  String get only_video => '只能添加一个视频';
+
+  @override
+  String get only_file => '只能添加一个文件';
+
+  @override
+  String get message_been_deleted => '消息已删除';
+
+  @override
+  String get back_lg => '账户在其他设备上登录';
+
+  @override
+  String get clear_history_confirm => '您确定要删除此聊天记录吗？';
+
+  @override
+  String get reset_password_confirm => '您确定要重置此密码吗？';
+
+  @override
+  String get unable_send => '无法发送';
+
+  @override
+  String get label_welcome_web => '登录到ChatWEB';
+
+  @override
+  String get label_des_scan_qr => '扫描下面的二维码以快速安全地登录。';
+
+  @override
+  String get label_use_app_scanning => '使用ChatAPP应用扫描二维码';
+
+  @override
+  String get label_welcome_web_2 => '立即登录以使用浏览功能并与朋友连接。';
+
+  @override
+  String get label_login_by_qr => '通过QR码登录';
+
+  @override
+  String get label_error_username => '系统中不存在该账户。';
+
+  @override
+  String get label_sent_link => '我们会将链接发送到您之前提供的电话号码地址。';
+
+  @override
+  String get label_back_to_login => '返回登录';
+
+  @override
+  String get label_verify_otp => '验证OTP';
+
+  @override
+  String get label_have_sent_code => '我们已将代码发送到电话号码 ';
+
+  @override
+  String get label_enter_otp_code => '输入OTP码';
+
+  @override
+  String get label_reset_password_des => '确保您的密码包含大写和小写字母以及数字，以保护您的账户。';
+
+  @override
+  String get label_register => '注册';
+
+  @override
+  String get label_login_phone_number => '通过手机号码登录';
+
+  @override
+  String get label_preview_chat_setting => '选择适合你的界面模式';
+
+  @override
+  String get label_apply => '应用';
+}
---
+++ build/web/flutter_service_worker.js
@@ -0,0 +1,280 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"flutter_bootstrap.js": "6e74682c25302fb6c25df596fffaf332",
+"version.json": "dde495debe3cc0a1b15e49383182a457",
+"index.html": "06150f6f7b68435ee14479ee9f003ceb",
+"/": "06150f6f7b68435ee14479ee9f003ceb",
+"main.dart.js": "6ec078bd12ec1608b071b7e351670fbe",
+"flutter.js": "f393d3c16b631f36852323de8e583132",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "9020048893b264d13794ca9df923e1d8",
+"assets/asset/images/light_mode.svg": "833d222dbd2c54bb06c58d3b6f5a517a",
+"assets/asset/images/empty_chat_detail.svg": "608c22046b116e33e504508cc976322d",
+"assets/asset/images/ic_home_chat.png": "d67ddb6b442584ed03cac17a6b2af66e",
+"assets/asset/images/img_theme_login.png": "244605561834d439a43338b119944179",
+"assets/asset/images/gg_service_account.json": "853f29281a49341b3058a22f9742105f",
+"assets/asset/images/ic_notification.png": "018f917ffabe984225fae3dcd602268e",
+"assets/asset/images/bg_light.png": "757d53720bb1bde3804fb12324561822",
+"assets/asset/images/ic_language.png": "8b26b8c99eb7311518938bd87db55733",
+"assets/asset/images/img_defaut_avatar.png": "dd88c3ccbd9f296fb0ac92af35b20821",
+"assets/asset/images/ic_ud_profile.png": "8a34f739a635c13a53d7f56686b2301e",
+"assets/asset/images/dark_mode.svg": "adca0c4f70e50e951590c9a3c646e6cf",
+"assets/asset/images/empty_chat_darkmode.png": "52db3c5b6327ed4930f2ca4c0f61665c",
+"assets/asset/images/ic_log_out.png": "be2de24acdfe7d737a57b297f1a74e7f",
+"assets/asset/images/bg_dark.png": "a782bbaef4bc65efc97e76c695b86b50",
+"assets/asset/images/default.svg": "ebd1e8761e23b9b157b80705670a00bf",
+"assets/asset/images/empty_chat_lightmode.png": "9daad1ac8b1daca7cb98d65cffdfa9c8",
+"assets/asset/images/ic_contact.png": "581c98352fdd248c25f5aeb0fac4af0f",
+"assets/asset/images/img_embedded_qr.png": "5ed8f04085944d45d1894317e2e00d43",
+"assets/asset/images/ic_friend_request.png": "7f38fbb6f54da873473255d1940509f5",
+"assets/asset/images/ic_night_mode.png": "46991da2f5791e49941529ba655ec7ac",
+"assets/asset/images/auto_mode.svg": "fc18c0cb33ecc000c570c5763320767d",
+"assets/asset/images/ic_active_session.png": "5a9c1977e2d871095f53d601d2ebec2a",
+"assets/asset/images/logo.svg": "ee9a3610861203e9f91c321613e90f93",
+"assets/asset/images/ic_account_avatar.png": "ba53e492e346383fbe007c2c1145ed36",
+"assets/asset/images/ic_chat_setting.png": "72faef7f6d7819738bc46a04536c10bd",
+"assets/asset/json/importing.json": "bfdd8b7df7e7b9ed5912b3b0035a5506",
+"assets/asset/icons/like.svg": "7d0ceaa2e5ab4c7fcab60feb66c518f9",
+"assets/asset/icons/draw_7.svg": "9639e7f53d79cd5050bcdf5768b731ca",
+"assets/asset/icons/smiling_face_with_sunglasses.svg": "51a6530ee635b7baec772356015f343a",
+"assets/asset/icons/draw_6.svg": "c64078f795cf09fbf617bbbbd1fc2671",
+"assets/asset/icons/txt.svg": "a50b07ab5406e7707fa18f4c06ca38ac",
+"assets/asset/icons/flag_vietnam.svg": "e9cb27bac552ba71633e702e29c0c81e",
+"assets/asset/icons/flag_china.svg": "8867eebdbdd3799b4e98ab33df4c42fb",
+"assets/asset/icons/reply.svg": "555b7d82a21d6f171d89caa8930c387a",
+"assets/asset/icons/draw_4.svg": "4fd5d895f6924c40760249626de2e651",
+"assets/asset/icons/no_peding.svg": "e24f91b96ed0355de2098d3e743a36c8",
+"assets/asset/icons/docx.svg": "faab72943bd0950a04858f0414a463a2",
+"assets/asset/icons/flag_english.svg": "a1884ec2dabef417617183656db167b9",
+"assets/asset/icons/draw_5.svg": "7a993457f86e8cccf69dcbb5420f80ef",
+"assets/asset/icons/draw_1.svg": "378602d31531d5405606a9aeee561f0c",
+"assets/asset/icons/ic_new_message.svg": "17163c9700615641066cb00981c82114",
+"assets/asset/icons/hidden.svg": "734b82bb7569b90261475b16d52661af",
+"assets/asset/icons/pdfx.svg": "fcd8f19ffa1aa9f0ecfb1f4db203d1bd",
+"assets/asset/icons/draw_2.svg": "e111b9e4506b49e998237a3c8ed3fe9a",
+"assets/asset/icons/file.svg": "01a2ec23a145f280e347a6f8519461c5",
+"assets/asset/icons/select_image.svg": "f3c60220963625da61caf33dd5f9485b",
+"assets/asset/icons/draw_3.svg": "e3bb0cfae9a77afe6ac05372e4b35eeb",
+"assets/asset/icons/fire.svg": "3a157ee1f9c21990a9c4a2ed825f0866",
+"assets/asset/icons/un_pin.svg": "99946cc9e2ad0121de7c7bbe46204128",
+"assets/asset/icons/pin_note.svg": "54b33481f25b353f6de2733639de568a",
+"assets/asset/icons/no_accout.svg": "7df66c062cfc29eec1b0247b63ff9c75",
+"assets/asset/icons/group_add.svg": "99529e4282f91717055f167a66e6b94a",
+"assets/asset/icons/pin.svg": "bc46f006f6a155340df1ede5c06941a9",
+"assets/asset/icons/mic.svg": "74844990925a09738951d19b90852e03",
+"assets/asset/icons/copy.svg": "6abeb2bde9ef238394bc3cab50f5c9fe",
+"assets/asset/icons/image.svg": "8b4b395875f3c0fe5a5f15742f0c2c22",
+"assets/asset/icons/xlsm.svg": "8063ec7a21501dde44f206325dc7aabe",
+"assets/asset/icons/no_friend.svg": "075945ec93b6259a15147866d7bf3d83",
+"assets/asset/icons/forward_chat.svg": "e08dcfe0ab6dd7b575ecfc79fb02454c",
+"assets/asset/icons/camera.svg": "27ee6316315c0a4673c4f4022ae3c235",
+"assets/asset/icons/edit.svg": "0062fdc278dc758fe7360f1bb913863a",
+"assets/asset/icons/eye_login.svg": "e895a051743e4ee7f825065d30c11a64",
+"assets/asset/icons/video_pick.svg": "a385929763918535ba48bf69a261ba4e",
+"assets/asset/icons/no_results.svg": "8d17855563894b0864006ebdee975754",
+"assets/asset/icons/delete.svg": "0e61b4491a71274558549650f72fe997",
+"assets/asset/icons/loudly_crying_face.svg": "326df10372d62efbf7fa271fb0f1c519",
+"assets/asset/icons/multiple_select.svg": "dcd39f92353f00d5860b569b79f0395d",
+"assets/asset/icons/ic_new_group.svg": "2acdea1dd45517f77539f5212c79a152",
+"assets/asset/icons/file_picker.svg": "2ab9f048c214280147b84394f7b0e1f8",
+"assets/asset/icons/face_with_tears_of_joy.svg": "5bcaa5b641c8aaab84ee9a36d2ede58e",
+"assets/asset/icons/add_chat.svg": "83a1997e4b7d58d89dfc0abfec43962f",
+"assets/asset/icons/draw_8.svg": "8ffa2faad7ad88f9a88f676d0e3373c1",
+"assets/asset/icons/forward.svg": "a9f3d7585b643209acd2ab9eea08fee7",
+"assets/asset/icons/smiling_face_with_heart_eyes.svg": "10210b4e9ee524b6b41e916176627a04",
+"assets/asset/icons/heart.svg": "5985bd1868f287348d90b5c0b0392177",
+"assets/AssetManifest.json": "a0dfa604d53d485f40040ec0019f4a55",
+"assets/NOTICES": "bc9a7a04b034428230620987c5626f38",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/AssetManifest.bin.json": "161b80f5e591f24a609e55265e274cfe",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"assets/AssetManifest.bin": "5980ed860ab44567936dc8abd0906fd0",
+"assets/fonts/MaterialIcons-Regular.otf": "9ecdd9344e4abbaeffabca283040408f",
+"canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
+"canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
+"canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
+"canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
+"canvaskit/chromium/canvaskit.js.symbols": "a012ed99ccba193cf96bb2643003f6fc",
+"canvaskit/chromium/canvaskit.js": "671c6b4f8fcc199dcc551c7bb125f239",
+"canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
+"canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
+"canvaskit/canvaskit.wasm": "1f237a213d7370cf95f443d896176460",
+"canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
